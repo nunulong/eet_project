9ca9ae79ad63f1930e0056dc99dd3d67f92fbf85
nunulong <felix.wang.1026@gmail.com>
2015-11-18 16:31:01 -0500

    Store the data in the csv file

--- data_collection.rb
+++ data_collection.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'csv'
 
 # Fetch and parse HTML document
 doc = Nokogiri::HTML(open('http://bestgrouprealty.com/agents.aspx?id=59'))
@@ -9,20 +10,22 @@ sub_links = doc.css('table#GridAgents td a').map{|link| "http://bestgrouprealty.
 
 # Iterate the links
 rs = []
-sub_links.each do |link|
+sub_links[0..20].each do |link|
   pp = {}
   sub_doc = Nokogiri::HTML(open(link))
   pp[:name] = sub_doc.css("span#lblTitle").text
   pp[:phone] = sub_doc.css("span#lblPhone").text
   pp[:email] = sub_doc.css("a#HyperLinkEmail").text.gsub(' {at} ', '@')
+  puts pp.inspect
   rs << pp
 end
 
 # [{name: '', phone: '917', email: 'gmail.com'},{}]
-
-# rs.each do 
-#   word <<
-# end
-
+CSV.open("./file.csv", "wb") do |csv|
+  csv << %w(name phone email)
+  rs.each do |pp|
+    csv << [pp[:name], pp[:phone], pp[:email]]
+  end
+end
   ####
   # Search for nodes by xpath
